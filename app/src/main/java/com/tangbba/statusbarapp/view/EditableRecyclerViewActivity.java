8d2ba525821952e5f17140d26a41c7b422002632
이정재/앱기술개발팀/NP <jungjae.lee@nhnent.com>
2018-12-04 17:39:09 +0900

    - 편집용 액티비티

--- app/src/main/java/com/tangbba/statusbarapp/view/EditableRecyclerViewActivity.java
+++ app/src/main/java/com/tangbba/statusbarapp/view/EditableRecyclerViewActivity.java
@@ -1,6 +1,70 @@
 package com.tangbba.statusbarapp.view;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.Icon;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+
+import com.tangbba.statusbarapp.R;
+import com.tangbba.statusbarapp.adapter.EditableIconItemRecyclerViewAdapter;
 import com.tangbba.statusbarapp.base.BaseActivity;
+import com.tangbba.statusbarapp.model.IconItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EditableRecyclerViewActivity extends BaseActivity {
+
+    private static final String TAG = "EditableRecyclerViewActivity";
+    private static final String EXTRA_ICON_ITEMS = "iconItems";
+    private static final String EXTRA_MODIFY_ICON_ITEMS = "modifyIconItems";
+
+    public static Intent newIntent(Context context, List<IconItem> iconItems) {
+        Intent intent = new Intent(context, EditableRecyclerViewActivity.class);
+        intent.putParcelableArrayListExtra(EXTRA_ICON_ITEMS, (ArrayList<IconItem>) iconItems);
+
+        return intent;
+    }
+
+    private RecyclerView mRecyclerView;
+    private GridLayoutManager mGridLayoutManager;
+    private EditableIconItemRecyclerViewAdapter mAdapter;
+    private List<IconItem> mIconItems;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editable_recycler_view);
+
+        mIconItems = getIntent().getParcelableArrayListExtra(EXTRA_ICON_ITEMS);
+        findViewById(R.id.submit_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putParcelableArrayListExtra(EXTRA_MODIFY_ICON_ITEMS, (ArrayList<IconItem>) mAdapter.getDataProvider());
+                setResult(Activity.RESULT_OK, intent);
+                finish();
+            }
+        });
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        setupRecyclerView(mIconItems);
+    }
+
+    private void setupRecyclerView(List<IconItem> dataProvider) {
+        mAdapter = new EditableIconItemRecyclerViewAdapter(this, new ArrayList<IconItem>());
+        mRecyclerView.setAdapter(mAdapter);
+
+        int spanCount = 4;
+        mGridLayoutManager = new GridLayoutManager(this, spanCount);
+        mRecyclerView.setLayoutManager(mGridLayoutManager);
+
+        mAdapter.setDataProvider(dataProvider);
+    }
 }
--- app/src/main/java/com/tangbba/statusbarapp/view/RecyclerViewActivity.java
+++ app/src/main/java/com/tangbba/statusbarapp/view/RecyclerViewActivity.java
@@ -1,11 +1,14 @@
 package com.tangbba.statusbarapp.view;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 import com.tangbba.statusbarapp.R;
 import com.tangbba.statusbarapp.adapter.IconItemRecyclerViewAdapter;
@@ -19,6 +22,7 @@ import java.util.ArrayList;
 public class RecyclerViewActivity extends BaseActivity {
 
     private static final String TAG = "RecyclerViewActivity";
+    public static final int REQ_ICON_ITEM_LIST = 1000;
 
     public static Intent newIntent(Context context) {
         return new Intent(context, RecyclerViewActivity.class);
@@ -36,8 +40,26 @@ public class RecyclerViewActivity extends BaseActivity {
         setSystemUIFragLightStatusBar();
 
         setContentView(R.layout.activity_recycler_view);
+
         mHomeIcons = new HomeIcons(IconItemGenerator.getIconItems());
+        findViewById(R.id.edit_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = EditableRecyclerViewActivity.newIntent(RecyclerViewActivity.this, mHomeIcons.clone());
+                startActivityForResult(intent, REQ_ICON_ITEM_LIST);
+            }
+        });
         setupRecyclerView();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQ_ICON_ITEM_LIST) {
+            if (resultCode == Activity.RESULT_OK) {
+
+            }
+        }
     }
 
     private void setupRecyclerView() {
